997ad70b3237e838acef7e69559d04d088c23b03
Karl Kihlström <mrkihlstrom@gmail.com>
2019-12-19 19:28:58 +0100

    :zap: Improve google font speed

--- src/main.js
+++ src/main.js
@@ -6,4 +6,20 @@ import inViewportDirective from 'vue-in-viewport-directive'
 export default function (Vue, { router, head, isClient }) {
   Vue.component('Layout', DefaultLayout)
   Vue.directive('in-viewport', inViewportDirective)
+
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700&display=swap'
+  })
+
+  head.link.push({
+    rel: 'dns-prefetch',
+    href: '//fonts.googleapis.com'
+  })
+
+  head.link.push({
+    rel: 'preconnect',
+    href: 'https://fonts.gstatic.com/',
+    crossorigin: '',
+  })
 }
